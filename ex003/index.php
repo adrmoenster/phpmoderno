e43b825c314b0e59fa1495dd5f74e5ecfa96c78f
Adriano Schulter moenster <adrmoenster@gmail.com>
2023-02-22 17:20:02 -0300

    manipulação de strings

--- ex003/index.php
+++ ex003/index.php
@@ -18,8 +18,34 @@
         $valor2 = 4.5;
         var_dump($valor2);
 
+        //TRABALHANDO COM STRINGS
         $valor3 = "Adriano";
         var_dump($valor3);
+
+        $nome = "Adriano"."\u{1F596}";//o ponto . faz concatenação e por ter aspas duplas "" ele analiza o conteudo da string, se fosse aspas simples '' mostraria só texto, mas não o código
+        echo "O meu nome é $nome";
+        echo 'O meu nome é $nome';// aqui não mostra conteúdo da váriável
+
+        //OBS não colocar Aspas duplas dentro de outras aspas duplas e nem colocar aspas simples dentro de outras aspas simples.
+        //Forma de usar aspas duplas dentro de aspas duplas é com sequência de escape assim o interpretador ira reconhecer, deve usar contrabarra \ exemplo echo "$nome\"Schulter\"Moenster";
+        
+        /*
+        Algumas sequências de escape para aspas duplas são
+        \n      Nova linha
+        \t      Tabulação horizontal
+        \\      Barra invertida 
+        \$      Sinal de cifrão
+        \u{}    Codepoint unicode   
+        
+        */
+
+        //CONSTANTE
+        const ESTADO = "PR";//Constante não usa $ e aconselhado em maiuscula
+        echo "Moro do ESTADO";//Não vai mostrar valor da contasnte ESTADO
+        echo 'Moro do ESTADO';//Não vai mostrar valor da contasnte ESTADO
+        echo "Moro no". ESTADO;// Aqui mostra valor da constante ou seja PR
+
+
 
         $valor4 = 3e2;//Aqui é igual a 3x10 elevado a 2 que é float de valor 300, por usar potencia ele concidera tipo float
         var_dump($valor4);
@@ -38,6 +64,7 @@
         $vetor = [6, 2.5, false, "Adriano"];
         var_dump($vetor);//mostra que é um array e mostra cada valor sua posição e seu tipo primitivo
 
+        //Aqui trabalhando com classe e como instância objeto
         // Object
         class Pessoa{
             private string $nome;
